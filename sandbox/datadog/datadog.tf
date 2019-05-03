c5ea48156d787559bfd8d9c4479cbe5a071ea5cf
Aaron <aaronjameshorne57@gmail.com>
2019-05-03 15:15:48 -0400

    update external id

--- sandbox/datadog/datadog.tf
+++ sandbox/datadog/datadog.tf
@@ -1,5 +1,5 @@
 module "datadog" {
 
     source = "s3::https://s3.amazonaws.com/chewy-cloudeng-tf-modules/datadog/1.2/module.zip" # from s3
-    datadog_external_id = "75754e709d8640fb84e76c2e4abc651c"
+    datadog_external_id = "c334b176bbcb47888861016ac30ec824"
 }
